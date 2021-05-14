2adb8b37cf6bd965e603e0e2393722d7dba74fc4
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-05-13 20:28:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-q
+//AIzaSyDd6ApZHAkVSZUKA7pecy1iox0tXs_gyf0 (api code)
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const { db } = require('./comments');
+import axios from 'axios';
+
+
+
+MongoClient.connect('mongodb+srv://koby:vang@cluster0.g5zz2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then(client => {
+    console.log('connected 1')
+    const db = client.db('youtube')
+    const youtubeMusic = db.collection('videos')
+})
+
+app.use (bodyParser.urlencoded({ extended: true }))
+
+app.listen(4000 , function() {
+    console.log('listening on 4000')
+})
+
+app.get('/', (req, res) => {
+    db.collection('videos').find().toArray()
+    .then(results => {
+        console.log(results)
+    })
+    .catch(error => console.error(error))
+})
+
+app.post('/videos', (req, res) => {
+    youtubeMusic.insertOne(req.body)
+        .then(result => {
+            res.direct('/')
+    })
+    .catch(error => console.error(error))
+})
