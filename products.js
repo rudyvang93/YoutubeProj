23dc3b8b3d197ecaf8605a90ef9ad88434a18ac1
Rudy Vang <80075567+rudyvang93@users.noreply.github.com>
2021-05-19 18:33:14 -0500

    Add files via upload

---
+++ products.js
@@ -0,0 +1,7 @@
+const Product = require('../models/product');
+const express = require('express');
+const router = express.Router();
+
+// All endpoints and route handlers go here
+
+module.exports = router;
